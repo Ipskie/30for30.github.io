15abc85a354637675d853b4ae9a5a61ce9296e8a
Secret Asian Man 3 <elijahyap@Secrets-MacBook-Pro.local>
2019-10-12 18:42:31 +0800

    change imports to ES imports

---
+++ imports.js
@@ -0,0 +1,132 @@
+
+export var navbar =
+`
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+    <div class="container">
+        <a class="navbar-brand" href="../index.html">30for30</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive"
+            aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarResponsive">
+            <ul id="nav-options" class="navbar-nav ml-auto">
+                <!-- nav-options go here -->
+                </ul>
+        </div>
+    </div>
+</nav>
+`
+export var nav_home =
+`
+    <div id="nav-home">
+        <li class="nav-item">
+            <a class="nav-link active" href="../index.html">Home
+            <span class="sr-only">(current)</span>
+            </a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../articles.html">Articles</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../contribute.html">Contribute</a>
+        </li>
+        <li class="nav-item">
+            <a class="nav-link" href="../data.html">Data</a>
+        </li>
+    </div>
+`
+
+export var nav_articles =
+`
+<li class="nav-item">
+    <a class="nav-link" href="../index.html">Home
+        <span class="sr-only">(current)</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link active" href="../articles.html">Articles</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../contribute.html">Contribute</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../data.html">Data</a>
+</li>
+`
+export var nav_contribute =
+`
+<li class="nav-item">
+    <a class="nav-link" href="../index.html">Home
+        <span class="sr-only">(current)</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../articles.html">Articles</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link active" href="../contribute.html">Contribute</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../data.html">Data</a>
+</li>
+`
+
+export var nav_data =
+`
+<li class="nav-item">
+    <a class="nav-link" href="../index.html">Home
+        <span class="sr-only">(current)</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../articles.html">Articles</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../contribute.html">Contribute</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link active" href="../data.html">Data</a>
+</li>
+`
+
+export var nav_none =
+`
+<li class="nav-item">
+    <a class="nav-link" href="../index.html">Home
+        <span class="sr-only">(current)</span>
+    </a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../articles.html">Articles</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../contribute.html">Contribute</a>
+</li>
+<li class="nav-item">
+    <a class="nav-link" href="../data.html">Data</a>
+</li>
+`
+
+export var contribute =
+`
+<div class="article-card rounded-lg">
+    <div class="rounded-lg" id="contribute-teaser">
+        <h2 class='text-white'>Contribute Today!</h2>
+        <p class='text-white'>Free, Public Data</p>
+        <button href="../contribute.html" class="btn btn-primary btn-sm">Check It Out</button>
+    </div>
+    <a href="../contribute.html"><span class="article-link"></span></a>
+</div>
+`
+export var footer = 
+`
+<!-- Footer Template -->
+<footer class="py-3 bg-dark">
+    <div class="container">
+        <div class="row">
+            <div class="copyright">Copyright &copy; 30for30 2019</div>
+            <div class="m-0 text-right text-white">Other Stuff</div>
+        </div>
+    </div>
+</footer>
+`
